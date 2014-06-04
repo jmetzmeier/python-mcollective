ef7d59edeee1179248dc24a9e61260a7c0f390fd
Jordan Metzmeier <=>
2014-06-04 15:52:49 -0500

    Work with plugin.activemq.base64 configurations Fixes #17
    * cleans up flak8 errors
    * decodes messages when returned
    * support deserializing !ruby/syn Fixes #13

--- pymco/serializers/yaml.py
+++ pymco/serializers/yaml.py
@@ -15,10 +15,22 @@ except ImportError as exc:
     raise exc
 
 
+def construct_ruby_object(loader, suffix, node):
+    return loader.construct_yaml_map(node)
+
+
+def construct_ruby_sym(loader, node):
+    return loader.construct_yaml_str(node)
+
+
 class Serializer(SerializerBase):
+    def __init__(self):
+        yaml.add_multi_constructor(u"!ruby/object:", construct_ruby_object)
+        yaml.add_constructor(u"!ruby/sym", construct_ruby_sym)
+
     """YAML specific serializer."""
     def serialize(self, msg):
         return yaml.safe_dump(dict(msg))
 
     def deserialize(self, msg):
-        return yaml.safe_load(msg)
+        return yaml.load(msg)
